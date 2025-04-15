75f5635aedb43e62287af3fd5e872a5f89f3c086
Deepak Garg <mailto.deepakgarg@gmail.com>
2025-04-15 09:27:45 -0700

    Add initial project files

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Express MongoDB API' });
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
